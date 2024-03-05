ae0857e75b16a63e81caeb6db33b7f7beb2b4c9f
FerdaouesMalek <dadousamalek@gmail.com>
2024-03-05 12:12:24 +0100

    adding files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// App.js
+import React from "react";
+import PlayersList from "./PlayersList";
+
+function App() {
+  return (
+    <div className="App">
+      <PlayersList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/players.js
@@ -0,0 +1,38 @@
+// players.js
+const players = [
+    {
+      name: "Lionel Messi",
+      team: "Paris Saint-Germain",
+      nationality: "Argentine",
+      jerseyNumber: 30,
+      age: 34,
+      imageUrl: "https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blt12dbddde5342ce4c/648866ff21a8556da61fa167/GOAL_-_Blank_WEB_-_Facebook_-_2023-06-13T135350.847.png?auto=webp&format=pjpg&width=3840&quality=60",
+    },
+    {
+      name: "Cristiano Ronaldo",
+      team: "Manchester United",
+      nationality: "Portuguese",
+      jerseyNumber: 7,
+      age: 37,
+      imageUrl: "https://upload.wikimedia.org/wikipedia/commons/d/d7/Cristiano_Ronaldo_playing_for_Al_Nassr_FC_against_Persepolis%2C_September_2023_%28cropped%29.jpg",
+    },
+    {
+      name: "Neymar Jr",
+      team: "Paris Saint-Germain",
+      nationality: "Brazilian",
+      jerseyNumber: 10,
+      age: 30,
+      imageUrl: "https://upload.wikimedia.org/wikipedia/commons/b/bb/Neymar_Jr._with_Al_Hilal%2C_3_October_2023_-_03_%28cropped%29.jpg",
+    },
+    {
+      name: "Kevin De Bruyne",
+      team: "Manchester City",
+      nationality: "Belgian",
+      jerseyNumber: 17,
+      age: 30,
+      imageUrl: "https://media.ouest-france.fr/v1/pictures/MjAyMzEyNWU1OGQxZmY3OWVlOGVkM2RjMzZiMTFmN2IwYWM0ZmE?width=1260&height=708&focuspoint=50%2C25&cropresize=1&client_id=bpeditorial&sign=22d7896b59982a1007dd21c3bd41156f72b75ceaea84ab6f4f5c4298760543a8cd ",
+    },
+  ];
+  
+  export default players;
+  
